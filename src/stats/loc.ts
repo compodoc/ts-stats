969e9d4700407415b123b2a8a61620ab24ab2c24
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-11 22:58:27 +0200

    fix: avoid NaN values

--- src/stats/loc.ts
+++ src/stats/loc.ts
@@ -5,38 +5,50 @@ const KEY3 = 'LOC (mode)';
 
 export default function(sourcesFiles: SourceFile[]) {
   const lineNumbers = [];
-  const count = {};
+  const loc = {};
+  let mean = '0';
+  let mode = '0';
+  let median = '0';
   sourcesFiles
     .map(sourceFile => {
-      const endLineNumber = sourceFile.getEndLineNumber();
+      // count LOC per file
+      // file #1 [0] = 123
+      // file #2 [1] = 987
+      // file #3 [2] = 26
+      // file #4 [3] = 9903
+      const endLineNumber = sourceFile.getEndLineNumber() - 1;
       lineNumbers.push(endLineNumber);
 
       // group by number of lines of code:
       // eg. 2000 (key) lines of code appeared in 1 (value) file
       // eg. 87 (key) lines of code appeared in 23 (value) files
       // eg. 255 (key) lines of code appeared in 99 (value) files
-      count[endLineNumber] = (count[endLineNumber] || 0) + 1;
+      loc[endLineNumber] = (loc[endLineNumber] || 0) + 1;
     })
     .sort();
 
-  // avg
-  const avg = (lineNumbers.reduce((a, b) => a + b, 0) / lineNumbers.length).toFixed(2);
+  // mean
+  if (lineNumbers.length > 0) {
+    mean = (lineNumbers.reduce((a, b) => a + b, 0) / lineNumbers.length).toFixed(2);
 
-  // median
-  let middle = lineNumbers.length / 2;
-  let median = lineNumbers[middle];
-  if (lineNumbers.length % 2 === 0) {
-    median = ((lineNumbers[middle] + lineNumbers[middle + 1]) / 2).toFixed(2);
+    // median
+    let middle = (lineNumbers.length / 2) | 0;
+    median = lineNumbers[middle].toFixed(2);
+    if (lineNumbers.length % 2 === 0) {
+      median = ((lineNumbers[middle] + lineNumbers[middle + 1]) / 2).toFixed(2);
+    }
+
+    // mode
+    // sort by values
+    mode = Object.keys(loc)
+      .map(Number)
+      .sort((a, b) => loc[a] - loc[b])
+      .pop()
+      .toFixed(2);
   }
-
-  // mode
-  // sort by values
-  const mode = Object.keys(count).sort( (a,b) => count[a] - count[b] ).pop();
 
   return {
     keys: [[KEY1, KEY2, KEY3].join('\n')],
-    values: [
-      [avg, median, mode].join('\n')
-    ]
+    values: [[mean, median, mode].join('\n')]
   };
 }
