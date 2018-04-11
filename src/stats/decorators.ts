1413c7c62130a7da789a1e2c0e94cf813dff8acf
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-11 22:58:56 +0200

    feat: add properties, method, constructor args decorators

--- src/stats/decorators.ts
+++ src/stats/decorators.ts
@@ -1,17 +1,42 @@
 import { SourceFile } from 'ts-simple-ast';
 
+function pushDecorator(classe, decorator, stats) {
+  const _decorator = `@${decorator.getFullName()}()`;
+  const entry = stats[_decorator];
+  if (!entry) {
+    stats[_decorator] = [];
+  }
+  stats[_decorator].push(classe.getName());
+}
+
 export default function(sourcesFiles: SourceFile[]) {
   const stats = {};
   sourcesFiles.map(sourceFile => {
     sourceFile.getClasses().map(classe => {
       try {
+        // class decorators
         classe.getDecorators().map(decorator => {
-          const _decorator = `@${decorator.getFullName()}`;
-          const entry = stats[_decorator];
-          if (!entry) {
-            stats[_decorator] = [];
-          }
-          stats[_decorator].push(classe.getName());
+          pushDecorator(classe, decorator, stats);
+        });
+        // property decorators
+        classe.getProperties().map(property => {
+          property.getDecorators().map(decorator => {
+            pushDecorator(classe, decorator, stats);
+          })
+        });
+        // method decorators
+        classe.getMethods().map(method => {
+          method.getDecorators().map(decorator => {
+            pushDecorator(classe, decorator, stats);
+          })
+        });
+        // constructor args decorators
+        classe.getConstructors().map(constructor => {
+          constructor.getParameters().map(parameter => {
+            parameter.getDecorators().map(decorator => {
+              pushDecorator(classe, decorator, stats);
+            })
+          })
         });
       } catch (e) {}
     });
