81e655ad6019c13df2f5c23a5601ea73b952df70
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-12 00:40:50 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,62 @@
 #!/usr/bin/env node
 import * as path from 'path';
 import * as fs from 'fs';
+const ora = require('ora');
 import { fork } from 'child_process';
 const forked = fork(path.join(__dirname, './worker.js'));
 
-var argv = require('minimist')(process.argv.slice(2), {
-  default: { p: 'tsconfig.json' }
-});
-const tsConfigFilePath = path.join(process.cwd(), argv.p);
-if (!fs.existsSync(tsConfigFilePath)) {
-  console.warn(`"${tsConfigFilePath}" file not found. Abort.`);
+// available stats
+// set to "true" to enable by default
+const availableStats = {
+  decorators: true,
+  classes: true,
+  interfaces: true,
+  imports: true,
+  exports: true,
+  specs: true,
+  loc: true,
+  comments: true,
+  files: true
+};
+const cliArgsDefaults = {
+  default: {
+    p: 'tsconfig.json',
+    ...availableStats
+  }
+};
+var argv = require('minimist')(process.argv.slice(2), cliArgsDefaults);
+const spinner = ora();
+const availableStatsKeys = Object.keys(availableStats).sort();
+const userArgvStatsKeys = Object.keys(argv);
+console.log(userArgvStatsKeys);
+
+const userFilteredStats = userArgvStatsKeys.filter(key => availableStatsKeys.indexOf(key) >= 0);
+if (userFilteredStats.length === 0) {
+  console.warn(`Please choose at least one stat type to continue...`);
+  console.warn(`Available stats are:`);
+  availableStatsKeys
+    .map(stat => console.log(`  --${stat}`));
+  console.warn(`Abort.`);
   process.exit(1);
 } else {
-  console.log(`Found: ${tsConfigFilePath}`);
+  
+  const tsConfigFilePath = path.join(process.cwd(), argv.p);
+  
+  if (!fs.existsSync(tsConfigFilePath)) {
+    console.warn(`"${tsConfigFilePath}" file not found. Abort.`);
+    process.exit(1);
+  } else {
+    console.log(`Found: ${tsConfigFilePath}`);
+  }
+
+  const projectName = require(path.join(process.cwd(), './package.json')).name;
+
+  spinner.color = 'green';
+  spinner.text = `Scanning project${projectName ? ': ' + projectName : ' ...'}`;
+  spinner.start();
+
+  forked.send({ tsConfigFilePath, stats: userFilteredStats });
 }
-
-const projectName = require(path.join(process.cwd(), './package.json')).name;
-
-const ora = require('ora');
-let spinner = ora();
-spinner.color = 'yellow';
-spinner.text = `Scanning project${projectName ? ': ' + projectName : ' ...'}`;
-spinner.start();
-
-// prettier-ignore
-const stats = [
-  'decorators',
-  'classes',
-  'interfaces',
-  'imports',
-  'exports',
-  'specs',
-  'loc', 
-  'files', 
-];
-forked.send({ tsConfigFilePath, stats });
 
 forked.on('message', done => {
   if (done.result) {
