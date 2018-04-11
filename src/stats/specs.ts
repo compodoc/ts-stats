35bf22642b45c271345531a49989181000855b2a
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-11 22:58:09 +0200

    feat: add xdescribe, xit

--- src/stats/specs.ts
+++ src/stats/specs.ts
@@ -1,38 +1,45 @@
 import { SourceFile, SyntaxKind } from 'ts-simple-ast';
 const KEY1 = 'Spec (files)';
 const KEY2 = 'Spec (describe)';
+const KEY2x = 'Spec (xdescribe)';
 const KEY3 = 'Spec (it)';
+const KEY3x = 'Spec (xit)';
+const KEY4 = 'Spec (expect)';
+
+function filter(funcNames, by) {
+  return funcNames.filter(n => n === by).length
+}
 export default function(sourcesFiles: SourceFile[]) {
   const files = [];
   let describe = 0;
   let it = 0;
+  let xdescribe = 0;
+  let xit = 0;
+  let expect = 0;
   sourcesFiles.filter(file => file.getFilePath().endsWith('spec.ts')).map(sourceFile => {
     files.push(sourceFile);
 
-    // const describe = (sourceFile.getFullText().match(/describe.*\(/g) || []).length;
-    // const it = (sourceFile.getFullText().match(/it.*\(/g) || []).length;
-    // console.log('describe ', describe, ' it', it, sourceFile.getFilePath());
+    const funcNames = sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression).map((funcName, index) => {
+      let name = '';
+      try {
+        name = funcName.getExpression().print();
+      } catch (e) {
+        // anonymouse functions
+        name = `arrow_func_${index}`;
+      }
 
-    const call = sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression);
-    const names = call.map(c => {
-      try { 
-        return c.getExpression().getSymbol().getName();
-      }
-      catch(e) {
-        return '';
-      }
+      return name;
     });
 
-    describe += names.filter(n => n === 'describe').length;
-    it += names.filter(n => n === 'it').length;
+    describe += filter(funcNames, 'describe');
+    it += filter(funcNames, 'it');
+    xdescribe += filter(funcNames, 'xdescribe');
+    xit += filter(funcNames, 'xit');
+    expect += filter(funcNames, 'expect');
   });
 
   return {
-    keys: [
-      [KEY1, KEY2, KEY3].join('\n')
-    ],
-    values: [
-      [files.length, describe, it].join('\n')
-    ]
+    keys: [[KEY1, KEY2, KEY2x, KEY3, KEY3x, KEY4].join('\n')],
+    values: [[files.length, describe, xdescribe, it, xit, expect].join('\n')]
   };
 }
