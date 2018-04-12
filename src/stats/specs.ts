0b38f6ffec5e6d4b81e5e91cb21b18f9dfe20f45
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-12 22:36:09 +0200

    feat: add fdescribe() and fit()

--- src/stats/specs.ts
+++ src/stats/specs.ts
@@ -2,8 +2,10 @@ import { SourceFile, SyntaxKind } from 'ts-simple-ast';
 const KEY1 = 'Spec (files)';
 const KEY2 = 'Spec (describe)';
 const KEY2x = 'Spec (xdescribe)';
+const KEY2f = 'Spec (fdescribe)';
 const KEY3 = 'Spec (it)';
 const KEY3x = 'Spec (xit)';
+const KEY3f = 'Spec (fit)';
 const KEY4 = 'Spec (expect)';
 
 function filter(funcNames, by) {
@@ -15,6 +17,8 @@ export default function(sourcesFiles: SourceFile[]) {
   let it = 0;
   let xdescribe = 0;
   let xit = 0;
+  let fdescribe = 0;
+  let fit = 0;
   let expect = 0;
   sourcesFiles.filter(file => file.getFilePath().endsWith('spec.ts')).map(sourceFile => {
     files.push(sourceFile);
@@ -35,11 +39,13 @@ export default function(sourcesFiles: SourceFile[]) {
     it += filter(funcNames, 'it');
     xdescribe += filter(funcNames, 'xdescribe');
     xit += filter(funcNames, 'xit');
+    fdescribe += filter(funcNames, 'fdescribe');
+    fit += filter(funcNames, 'fit');
     expect += filter(funcNames, 'expect');
   });
 
   return {
-    keys: [[KEY1, KEY2, KEY2x, KEY3, KEY3x, KEY4].join('\n')],
-    values: [[files.length, describe, xdescribe, it, xit, expect].join('\n')]
+    keys: [[KEY1, KEY2, KEY2x, KEY2f, KEY3, KEY3x, KEY3f, KEY4].join('\n')],
+    values: [[files.length, describe, xdescribe, fdescribe, it, xit, fit, expect].join('\n')]
   };
 }
