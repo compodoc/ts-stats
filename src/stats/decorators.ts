b4139ab44b42e2319063c8907a7f2765ef6108a4
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-12 14:21:00 +0200

    refactor: avoid function hoisting

--- src/stats/decorators.ts
+++ src/stats/decorators.ts
@@ -1,15 +1,16 @@
 import { SourceFile, Node, TypeGuards } from 'ts-simple-ast';
 import { stat } from 'fs';
 
-function visit(decorator: string, stats: {[key: string]: number}) {
-  const _decorator = `@${decorator}()`;
-  const entry = stats[_decorator];
-  stats[_decorator] = (stats[_decorator] || 0) + 1;
-}
 
 export default function(sourceFiles: SourceFile[]) {
   const stats: {[key: string]: number} = {};
-
+  
+  function visit(decorator: string, stats: {[key: string]: number}) {
+    const _decorator = `@${decorator}()`;
+    const entry = stats[_decorator];
+    stats[_decorator] = (stats[_decorator] || 0) + 1;
+  }
+  
   for (const sourceFile of sourceFiles) {
     sourceFile.forEachDescendant(descendant => {
       if (TypeGuards.isDecorator(descendant)) {
